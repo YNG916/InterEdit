57b0e02b9078786b9365e2cb446dd4a7d45aeaac
Yebin Yang - MA-KPeng <yyang@14s35.anthropomatik.kit.edu>
2026-03-13 08:39:12 +0000

    update README.md

--- prepare/download_evaluation_model.sh
+++ prepare/download_evaluation_model.sh
@@ -3,5 +3,5 @@ cd ./eval_model/
 echo "The pretrained evaluation model will be stored in the 'eval_model' folder\n"
 # InterCilp
 echo "Downloading the evaluation model..."
-gdown https://drive.google.com/file/d/1bsEvaynJ2Gwpv6SBLJk7LtIrw7Hsu4xH/view?usp=drive_link
+gdown "https://drive.google.com/uc?id=1bsEvaynJ2Gwpv6SBLJk7LtIrw7Hsu4xH"
 echo "Evaluation model downloaded"
